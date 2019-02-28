8378bda20155685c843a5a72e3ce19f6086aceb6
brindab50 behal <brindab50@gmail.com>
2019-02-28 18:23:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(string)
-  
-  if string != string.upcase
+  if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
